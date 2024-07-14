29036e9abfa5beba2920802a51a605322934a0dc
Maastricht99 <19alexferrante99@gmail.com>
2024-07-14 16:38:25 +0200

    KAN-16: add config validation zod

--- backend/api-gateaway/src/app.module.ts
+++ backend/api-gateaway/src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from "@nestjs/common";
 import { AuthModule } from "./auth/auth.module";
+import { ConfigModule } from "@nestjs/config";
+import validateAndLoadConfig from "./config";
+
+const CORE_MODULES = [AuthModule];
+
+const INFRA_MODULES = [
+  ConfigModule.forRoot({
+    isGlobal: true,
+    load: [validateAndLoadConfig]
+  })
+]
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ...CORE_MODULES,
+    ...INFRA_MODULES
+  ],
   controllers: [],
   providers: [],
 })
