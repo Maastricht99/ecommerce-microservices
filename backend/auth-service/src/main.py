9befe41e00455e7eccd764517b1e8053a51e7b97
Maastricht99 <19alexferrante99@gmail.com>
2024-07-15 22:59:44 +0200

    KAN-21: update docker setup

--- backend/auth-service/src/main.py
+++ backend/auth-service/src/main.py
@@ -55,4 +55,4 @@ async def authenticate(request: Request):
     return payload
 
 if __name__ == "__main__":
-    uvicorn.run(app, port=config.SERVER_PORT)
+    uvicorn.run(app, host="0.0.0.0", port=config.SERVER_PORT)
